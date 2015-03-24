7d13a200ad9e426f7753b0c429795930d5b4f4b1
JIUNG SHIN <jushin17@gmail.com>
2015-03-24 15:34:01 +0900

    Calculator homework Kookmin Univ. Mobile/Cloud newest technology.

--- app/src/main/java/com/example/shinjiung/calculator_v1/MainActivity.java
+++ app/src/main/java/com/example/shinjiung/calculator_v1/MainActivity.java
@@ -3,12 +3,6 @@ package com.example.shinjiung.calculator_v1;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View.OnClickListener;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
@@ -214,49 +208,6 @@ public class MainActivity extends ActionBarActivity {
                     }
 
                 }
-
-
-
-                /*
-
-                else if (v==button_equal){
-                    if(check==1){
-
-                        result = Float.parseFloat(textValue1.getText().toString().trim()) % Float.parseFloat((textValue2.getText().toString().trim()));
-
-                        resultValue.setText(Float.toString(result));
-
-                    }
-                    if(check==2){
-
-                        result = Integer.valueOf(textValue1.getText().toString().trim()) / Integer.valueOf(textValue2.getText().toString().trim());
-
-                        resultValue.setText(Integer.toString(result));
-
-                    }
-                    if(check==3){
-
-                        result = Integer.valueOf(textValue1.getText().toString().trim()) + Integer.valueOf(textValue2.getText().toString().trim());
-
-                        resultValue.setText(Integer.toString(result));
-
-                    }
-                    if(check==4){
-
-                        result = Integer.valueOf(textValue1.getText().toString().trim()) - Integer.valueOf(textValue2.getText().toString().trim());
-
-                        resultValue.setText(Integer.toString(result));
-
-                    }
-                    if(check==5){
-
-                        result = Integer.valueOf(textValue1.getText().toString().trim()) * Integer.valueOf(textValue2.getText().toString().trim());
-
-                        resultValue.setText(Integer.toString(result));
-
-                    }
-
-                }*/
             }
         };
         button1.setOnClickListener(cl);
@@ -295,42 +246,3 @@ public class MainActivity extends ActionBarActivity {
         button_sub.setOnClickListener(cl);
     }
 }
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    /*
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }*/
